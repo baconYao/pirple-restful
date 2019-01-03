f09c3d113207b2772afe84992b8fb0a0361f3d0a
baconYao <bacon735392@gmail.com>
2019-01-04 00:35:14 +0800

    Implement parser to parse request url path

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const http = require("http");
+const url = require("url");
 
 // The server should response to all requests with a string
 const server = http.createServer(function(req, res) {
+  
+  // Get the URL and parse it
+  var parseUrl = url.parse(req.url, true);
+  
+  // Get the path
+  var path = parseUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Send response
   res.end("Hello World\n");
+
+  // Log the request path
+  console.log('Request received on path: ' + trimmedPath);
 });
 
 // Start the server, and have it listen on port 3000
